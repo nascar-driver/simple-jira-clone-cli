c9073da63ebfb6f9169f661ead293256fe95dc56
nascar-driver <jordanlaphon@gmail.com>
2026-06-01 01:54:23 +0700

    feat: read and write to json file

--- Stage 1/Step 2/src/models.rs
+++ Stage 1/Step 2/src/models.rs
@@ -1,14 +1,17 @@
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 // TODO: derive the appropriate traits
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 pub enum Status {
     Open,
     InProgress,
     Resolved,
-    Closed
+    Closed,
 }
 
 // TODO: derive the appropriate traits
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 pub struct Epic {
     pub name: String,
     pub description: String,
@@ -22,12 +25,13 @@ impl Epic {
             name,
             description,
             status: Status::Open,
-            stories: vec![]
+            stories: vec![],
         }
     }
 }
 
 // TODO: derive the appropriate traits
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 pub struct Story {
     pub name: String,
     pub description: String,
@@ -45,8 +49,9 @@ impl Story {
 }
 
 // TODO: derive the appropriate traits
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 pub struct DBState {
     pub last_item_id: u32,
     pub epics: HashMap<u32, Epic>,
-    pub stories: HashMap<u32, Story>
+    pub stories: HashMap<u32, Story>,
 }
